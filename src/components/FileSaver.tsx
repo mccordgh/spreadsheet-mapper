e9710049cc3c77601d11fd6638ae76594b592d33
Matthew McCord <mattm.eternal@gmail.com>
2025-03-20 16:49:05 -0700

    create FileSaver component

---
+++ src/components/FileSaver.tsx
@@ -0,0 +1,20 @@
+import React, { ChangeEvent } from 'react';
+
+import "../styles/FileSaver.css";
+
+type FileSaverProps = {
+    title: string;
+}
+
+const FileSaver = (props: FileSaverProps) => {
+  return (
+    <div className="file-saver--wrapper">
+        <label className="file-saver--header" htmlFor={`file-saver-${props.title}`}>
+            {props.title}
+        </label>
+        {/* <input className="file-saver--text" id={`file-saver-${props.title}`} onChange={props.onFileLoad} /> */}
+    </div>
+  );
+}
+
+export { FileSaver };
