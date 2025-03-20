19a7daf7b36d16d70d226eefdc6b50eae81b0203
Matthew McCord <mattm.eternal@gmail.com>
2025-03-20 16:03:55 -0700

    dummy data for quicker dev

--- src/App.tsx
+++ src/App.tsx
@@ -6,12 +6,16 @@ import { parse, ParseResult, LocalFile } from 'papaparse';
 
 import './App.css';
 
+// TODO: Remove when done testing
+const dummyHeaderListA: string[] = ['Token', 'Item Name', 'Variation Name', 'Unit and Precision', 'SKU', 'Description', 'Reporting Category', 'SEO Title', 'SEO Description', 'Permalink', 'GTIN', 'Square Online Item Visibility', 'Item Type', 'Weight (lb)', 'Social Media Link Title', 'Social Media Link Description', 'Shipping Enabled', 'Self-serve Ordering Enabled', 'Delivery Enabled', 'Pickup Enabled', 'Price', 'Online Sale Price', 'Archived', 'Sellable', 'Stockable', 'Skip Detail Screen in POS', 'Option Name 1', 'Option Value 1', 'Option Name 2', 'Option Value 2', 'Option Name 3', 'Option Value 3', 'Default Unit Cost', 'Default Vendor Name', 'Default Vendor Code', 'Current Quantity Erdé', 'New Quantity Erdé', 'Stock Alert Enabled Erdé', 'Stock Alert Count Erdé'];
+const dummyHeaderListB: string[] = ['Title', 'URL handle', 'Description', 'Vendor', 'Product category', 'Type', 'Tags', 'Published on online store', 'Status', 'SKU', 'Barcode', 'Option1 name', 'Option1 value', 'Option2 name', 'Option2 value', 'Option3 name', 'Option3 value', 'Price', 'Price / International', 'Compare-at price', 'Compare-at price / International', 'Cost per item', 'Charge tax', 'Tax code', 'Inventory tracker', 'Inventory quantity', 'Continue selling when out of stock', 'Weight value (grams)', 'Weight unit for display', 'Requires shipping', 'Fulfillment service', 'Product image URL', 'Image position', 'Image alt text', 'Variant image URL', 'Gift card', 'SEO title', 'SEO description', 'Google Shopping / Google product category', 'Google Shopping / Gender', 'Google Shopping / Age group', 'Google Shopping / MPN', 'Google Shopping / AdWords Grouping', 'Google Shopping / AdWords labels', 'Google Shopping / Condition', 'Google Shopping / Custom product', 'Google Shopping / Custom label 0', 'Google Shopping / Custom label 1', 'Google Shopping / Custom label 2', 'Google Shopping / Custom label 3', 'Google Shopping / Custom label 4'];
 
 function App() {
-  const [headerListA, setHeaderListA] = React.useState<string[]>([]);
-  const [headerListB, setHeaderListB] = React.useState<string[]>([]);
+  // TODO: Reset initial value to '[]' when done testing
+  const [headerListA, setHeaderListA] = React.useState<string[]>(dummyHeaderListA);
+  const [headerListB, setHeaderListB] = React.useState<string[]>(dummyHeaderListB);
 
-  const onFileLoad = (event: ChangeEvent<HTMLInputElement>): void => {
+  const onFileLoad = (event: ChangeEvent<HTMLInputElement>, callback: (fields: string[]) => void): void => {
     if (event?.target?.files) {
       const file: File = (event.target.files[0] as File);
 
@@ -20,7 +24,7 @@ function App() {
         complete: (results: ParseResult<File>, file: LocalFile): void => {
           if (results?.meta?.fields) {
             console.log('Fields:', results.meta.fields);
-            setHeaderListA(results.meta.fields);
+            callback(results.meta.fields);
           }
           // console.log('Parsing complete:', results);
         },
@@ -41,9 +45,9 @@ function App() {
 
       <div className="App-body">
         <div className="App-body-left">
-          <FileLoader title="Load File A" onFileLoad={onFileLoad}/>
+          <FileLoader title="Load File A" onFileLoad={(event: ChangeEvent<HTMLInputElement>) => { onFileLoad(event, setHeaderListA) }}/>
           <hr />
-          <HeaderList title="Columns in File A:" headers={headerListA} onClick={() => { console.log('header list clicked'); }}/>
+          <HeaderList title="Columns in File A:" headers={headerListA} onClick={() => { console.log('header list A clicked'); }}/>
         </div>
         
         <div className="App-body-center">
@@ -51,7 +55,9 @@ function App() {
         </div>
 
         <div className="App-body-right">
-          <FileLoader title="Load File B" onFileLoad={onFileLoad}/>
+          <FileLoader title="Load File B" onFileLoad={(event: ChangeEvent<HTMLInputElement>) => { onFileLoad(event, setHeaderListB) }}/>
+            <hr />
+          <HeaderList title="Columns in File B:" headers={headerListB} onClick={() => { console.log('header list B clicked'); }}/>
         </div>
       </div>
 
