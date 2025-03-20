32f556329446b7b4a884dd87d16b7c947de4b020
Matthew McCord <mattm.eternal@gmail.com>
2025-03-20 15:51:21 -0700

    add HeaderList component to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,38 @@
-import React from 'react';
+import React, { ChangeEvent } from 'react';
+import { FileLoader } from './components/FileLoader';
+import { HeaderList } from './components/HeaderList';
 
-import { Button } from './components/Button';
-import { ListItem } from './components/ListItem';
-import { MappingItem } from './components/MappingItem';
-import { TextBox } from './components/TextBox';
+import { parse, ParseResult, LocalFile } from 'papaparse';
 
 import './App.css';
 
+
 function App() {
+  const [headerListA, setHeaderListA] = React.useState<string[]>([]);
+  const [headerListB, setHeaderListB] = React.useState<string[]>([]);
+
+  const onFileLoad = (event: ChangeEvent<HTMLInputElement>): void => {
+    if (event?.target?.files) {
+      const file: File = (event.target.files[0] as File);
+
+      const config = {
+        header: true,
+        complete: (results: ParseResult<File>, file: LocalFile): void => {
+          if (results?.meta?.fields) {
+            console.log('Fields:', results.meta.fields);
+            setHeaderListA(results.meta.fields);
+          }
+          // console.log('Parsing complete:', results);
+        },
+        error: (error: unknown, file: LocalFile): void => {
+          console.error('Error parsing file:', error);
+        },
+      }
+  
+      parse<File>(file, config);
+    }
+  };
+
   return (
     <div className="App">
       <header className="App-header">
@@ -15,10 +40,19 @@ function App() {
       </header>
 
       <div className="App-body">
-        <TextBox />
-        <Button />
-        <ListItem />
-        <MappingItem />
+        <div className="App-body-left">
+          <FileLoader title="Load File A" onFileLoad={onFileLoad}/>
+          <hr />
+          <HeaderList title="Columns in File A:" headers={headerListA} onClick={() => { console.log('header list clicked'); }}/>
+        </div>
+        
+        <div className="App-body-center">
+          {/* <FileSaver title="Save New File"/> */}
+        </div>
+
+        <div className="App-body-right">
+          <FileLoader title="Load File B" onFileLoad={onFileLoad}/>
+        </div>
       </div>
 
       <div className="App-footer">
