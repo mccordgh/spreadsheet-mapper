85cf49daf97afdc73bc40d60d601c8728a8f8078
Matthew McCord <mattm.eternal@gmail.com>
2025-03-21 17:00:13 -0700

    create HeadersContext

---
+++ src/context/HeadersContext.ts
@@ -0,0 +1,16 @@
+import { createContext } from "react";
+import { HeaderItem } from "../models/HeaderItem";
+
+export type HeadersContextType = {
+    headersFrom: HeaderItem[];
+    headersTo: HeaderItem[];
+    headerClicked: (id: number, type: 'from' | 'to') => void;
+}
+
+export const HeadersContext = createContext<HeadersContextType>(
+    {
+        headersFrom: [],
+        headersTo: [],
+        headerClicked: (id: number, type: 'from' | 'to') => {},
+    }
+);
