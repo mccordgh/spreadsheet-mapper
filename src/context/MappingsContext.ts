d1763d2f86f44332bf680256df9c9e83c6b32939
Matthew McCord <mattm.eternal@gmail.com>
2025-03-21 17:00:23 -0700

    create MappingsContext

---
+++ src/context/MappingsContext.ts
@@ -0,0 +1,16 @@
+import { createContext } from 'react';
+import { HeaderMapping } from '../models/HeaderMapping';
+
+export type MappingsContextType = {
+    mappings: HeaderMapping[];
+    confirmMapping: (id: number) => void;
+    deleteMapping: (id: number) => void;
+}
+
+export const MappingsContext = createContext<MappingsContextType>(
+    {
+        mappings: [],
+        confirmMapping: (id: number) => {},
+        deleteMapping: (id: number) => {},
+    }
+);
